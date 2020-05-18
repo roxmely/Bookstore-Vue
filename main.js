4c1c8e38c3ec652b6a5e8372803f5488a1ffcf66
Roxana Melgarejo Yasila <r.melgarejo.ubiqum@gmail.com>
2020-05-18 21:55:28 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+let myApp = new Vue({
+  el: "#app",
+  data: {
+    url: "https://api.npoint.io/906c14ba38db9ecc9e8d",
+    books: [],
+    search: "",
+  },
+  methods: {
+    cogerLibros() {
+      fetch(this.url)
+        .then(res => res.json())
+        .then(json => (this.books = json.books));
+    },
+  },
+  computed: {
+    filterBooks() {
+      // .filter me  crea un nuevo array con los elementos que cumplan la condición de la función.
+      return this.books.filter(book => {
+        return (
+          // .match devuelve las coincidencias dentro de una cadena, en este caso busca las
+          // coincidencias de titulo dentro de search.
+          // toUpperCase me devuelve el valor el vakor de la cadena en mayúsculas.
+          book.titulo.toUpperCase().match(this.search.toUpperCase()) ||
+          book.descripcion.toUpperCase().match(this.search.toUpperCase())
+        );
+      });
+    },
+  },
+  //  inicia   variables del estado de manera asíncrona
+  created() {
+    this.cogerLibros();
+  },
+});
